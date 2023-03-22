f1ca4653e66e237eb068a1866061951975fcfba5
ssooya <ssooya90@naver.com>
2023-03-23 00:41:08 +0900

    객체 맵핑

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDRES")
+public class Order {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ORDER_ID")
+	private Long id;
+
+	@Column(name ="MEMBER_ID")
+	private Long memberId;
+
+	private Member member;
+
+	private LocalDateTime orderDate;
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+}
